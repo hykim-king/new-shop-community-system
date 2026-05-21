4d5bcc9b079ba8f48d262c10670c1761c62cfa64
wholesome-gee <jiyong0419@naver.com>
2026-05-21 16:46:08 +0900

    Comment 기능 구현

---
+++ src/com/company/shop/domain/CommentVO.java
@@ -0,0 +1,14 @@
+/**
+ * 파일명: CommentVO.java <br>
+ * 작성자: Wholesome-Gee  <br>
+ * 생성일: 2026-05-21 <br>
+ * 설　명: <br>
+ */
+package com.company.shop.domain;
+
+/**
+ * 
+ */
+public class CommentVO {
+
+}
