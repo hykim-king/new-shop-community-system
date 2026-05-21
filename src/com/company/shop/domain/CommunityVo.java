e2658eed97447033cd9a939c0f7ffc97898ae959
jayoung <wkdwodud97@gmail.com>
2026-05-21 17:05:12 +0900

    feat 커뮤니티 완성

---
+++ src/com/company/shop/domain/CommunityVo.java
@@ -0,0 +1,23 @@
+/**
+ * 파일명: CommunityVo.java <br>
+ * 설명 :   <br>
+ * 작성자: User  <br>
+ * 생성일: 2026-05-21 <br>
+ * 버전 : 1.0
+ */
+package com.company.shop.domain;
+
+/**
+ * 
+ */
+public class CommunityVo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/company/shop/service/CommunityService.java
@@ -0,0 +1,15 @@
+/**
+ * 파일명: CommunityService.java <br>
+ * 설명 :   <br>
+ * 작성자: User  <br>
+ * 생성일: 2026-05-21 <br>
+ * 버전 : 1.0
+ */
+package com.company.shop.service;
+
+/**
+ * 
+ */
+public class CommunityService {
+
+}
