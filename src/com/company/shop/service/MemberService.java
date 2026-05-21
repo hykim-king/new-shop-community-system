25bfce95422a85d5d5bc6facef6bf11a810bda8b
EHKang <eunhugang@gmail.com>
2026-05-21 17:13:22 +0900

    feat : 강은후의 깃허브 테스트~

---
+++ src/com/company/shop/service/MemberService.java
@@ -0,0 +1,14 @@
+/**
+ * 파일명: MemberService.java <br>
+ * 설명 :   <br>
+ * 작성자: CannedCorn  <br>
+ * 생성일: 2026-05-21 <br>
+ */
+package com.company.shop.service;
+
+/**
+ * 
+ */
+public class MemberService {
+
+}
---
+++ src/com/company/shop/view/MemberView.java
@@ -0,0 +1,14 @@
+/**
+ * 파일명: MemberView.java <br>
+ * 설명 :   <br>
+ * 작성자: CannedCorn  <br>
+ * 생성일: 2026-05-21 <br>
+ */
+package com.company.shop.view;
+
+/**
+ * 
+ */
+public class MemberView {
+	
+}
